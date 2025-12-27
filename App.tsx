4aa25c1fe2e938b612909d096fe3675968062a6b
BHUPESH SANTHA KUMAR <65531119+Bhupesh360@users.noreply.github.com>
2025-12-27 22:33:00 +0530

    App.tsx RootNavigator Error SOlved

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import RootNavigator from './src/navigation/RootNavigator';
-import { ThemeProvider } from './src/context/ThemeContext';
-import { AuthProvider } from './src/context/AuthContext';
+import AppNavigator from './CubeAI/src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <SafeAreaProvider>
-          <StatusBar style="auto" />
-          <RootNavigator />
-        </SafeAreaProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <StatusBar style="dark" />
+      <AppNavigator />
+    </SafeAreaProvider>
   );
 }
